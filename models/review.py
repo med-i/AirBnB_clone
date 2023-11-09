013cb8cfe525a90044b3252a7e03efd347b049dd
meedouazane <mohamedouazane@yahoo.com>
2023-11-09 11:25:41 -0800

    amenity.py city.py place.py review.py state.py user.py

--- models/review.py
+++ models/review.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+''' review class that inherit from BaseModel '''
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    ''' review class that inherit from BaseModel '''
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        ''' Initializition. '''
+        super().__init__(self, *args, **kwargs)
