889491a0d2707bf183ba64940494409ccc0e62db
meedouazane <mohamedouazane@yahoo.com>
2023-11-11 07:22:51 -0800

    tests

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Unit tests for the BaseModel class.
+"""
+import unittest
+from models.base_model import BaseModel
+import uuid
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test cases for BaseModel class.
+    """
+    pass
