75db086ec9352ae5cff894e7b69c53cc2c753208
meedouazane <mohamedouazane@yahoo.com>
2023-11-13 14:24:45 -0800

    adding tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -4,13 +4,28 @@ Unit tests for the BaseModel class.
 """
 import unittest
 from models.base_model import BaseModel
-import uuid
-from datetime import datetime
 from models.city import City
 
 
 class TestCity(unittest.TestCase):
-    """
-    Test cases for BaseModel class.
-    """
-    pass
+    ''' test city user '''
+
+    def testname(self):
+        ''' test first name'''
+        City.name = "Messi"
+        self.assertEqual(City.name, "Messi")
+
+    def testnameEmpty(self):
+        ''' test first name empty '''
+        City.name = ""
+        self.assertEqual(City.name, "")
+
+    def testState_id(self):
+        ''' test first name'''
+        City.state_id = "Messi"
+        self.assertEqual(City.state_id, "Messi")
+
+    def testState_idEmpty(self):
+        ''' test first name empty '''
+        City.State_id = ""
+        self.assertEqual(City.State_id, "")
